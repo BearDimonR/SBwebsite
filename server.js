a018019738be028caa46d0ce238464e8d9de8689
BearDimonR <48130903+BearDimonR@users.noreply.github.com>
2020-08-27 01:54:17 +0300

    server.js fix

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist/sbwebsite'));
 
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+'/dist/sbwebsite/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/sbwebsite/src/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
